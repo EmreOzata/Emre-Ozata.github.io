88a2afb1965a03275be29c6a07bf9eb71e77be32
Emre Ozata <emre10909@gmail.com>
2025-09-15 14:45:51 +0200

    ändrat mailen

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section id="contact">
       <h2>Kontaktuppgifter</h2>
-      <address>Kontakta mig <a href="mailto:emre10909@gmail.com">här</a>.</address>
+      <address>Kontakta mig <a href="mailto:ozata.emre@protonmail.com">här</a>.</address>
     </section>
   );
 }
